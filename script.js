34a8642ec382b808fa1753106a9ebe4158bb4248
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2025-04-15 15:41:23 +0000

    add final touches to the UI

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ toggleBtn.addEventListener("click", () => {
 		themeToggle.src = "./assets/images/icon-moon.svg";
 	}
 });
+
+// LAB THREE (3)
+
+// Selecting the Elements involved in dynamism...
+const textBox = document.getElementById("text-box");
+const excludedSpaces = document.getElementById("exclude-spaces");
+const characterLimitChecker = document.getElementById("character-limit");
+const characterLimitSetter = document.getElementById("set-limit");
+const readTime = document.getElementById("read-time");
+const charCount = document.getElementById("char-count");
+const wordCount = document.getElementById("word-count");
+const sentenceCount = document.getElementById("sentence-count");
+const densityDetails = document.getElementById("density-details-filled");
+
+// Listening for Input Changes
